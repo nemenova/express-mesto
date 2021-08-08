fa4349751dbdb91b78fe287d184d4a1c0ac8abe9
Victoria Nemenova <vikalangor@gmail.com>
2021-08-08 09:08:42 +0300

    for lesha

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { PORT = 3000 } = process.env;
 const mongoose = require('mongoose');
+const userRoute = require('./routes/users');
+const bodyParser = require('body-parser');
 
+const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
@@ -9,6 +11,21 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useCreateIndex: true,
     useFindAndModify: false
 });
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: ' ' // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+
+  next();
+});
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(bodyParser.json()); // для собирания JSON-формата
+app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
+
+app.use('/users', userRoute)
 
 app.listen(PORT, () => {
     // Если всё работает, консоль покажет, какой порт приложение слушает
